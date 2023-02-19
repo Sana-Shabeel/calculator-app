55d064abfc1c95b73defa6e93aaa9e9b4997d836
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-19 08:08:54 +0300

    Add header comp

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Theme from "./Theme";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>calc</h1>
+      <div>
+        Theme <Theme />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/header/Theme.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Theme = () => {
+  return (
+    <div>
+      <div>
+        <p>1</p>
+        <p>2</p>
+        <p>2</p>
+      </div>
+      <div className="dot-container">
+        <span className="dot"></span>
+        <span className="dot"></span>
+        <span className="dot"></span>
+      </div>
+    </div>
+  );
+};
+
+export default Theme;
