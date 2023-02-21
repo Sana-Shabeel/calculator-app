dadd8f865a54516d7685fc82fb10a02e6926e17f
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-21 17:02:24 +0300

    Add theme switch functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,26 @@ import "./App.scss";
 import Keys from "./components/keypad/Keys";
 
 function App() {
+  const [toggle, setToggle] = useState("theme1");
+
+  const switchThemeHandler = (num: string) => {
+    switch (num) {
+      case "1":
+        setToggle("theme1");
+        break;
+      case "2":
+        setToggle("theme2");
+        break;
+      case "3":
+        setToggle("theme3");
+        break;
+    }
+  };
+
   return (
-    <div className="App theme1">
+    <div className={`${toggle} App`}>
       <div className="main">
-        <Header />
+        <Header switchThemeHandler={switchThemeHandler} />
         <div className="screen"></div>
         <Keys />
       </div>
