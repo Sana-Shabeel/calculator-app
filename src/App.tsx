f84399d0ab51ff87e3644043370ecc4ad49fbac7
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-21 13:58:23 +0300

    Add screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import Header from "./components/header/Header";
 import "./App.scss";
+import Keys from "./components/keypad/Keys";
 
 function App() {
   return (
     <div className="App theme1">
       <div className="main">
         <Header />
+        <div className="screen"></div>
+        <Keys />
       </div>
     </div>
   );
