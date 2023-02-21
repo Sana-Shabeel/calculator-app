071e67df5dc4ec67f09c3cf79121b9c1cbae396f
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-21 23:14:18 +0300

    Add calculation functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Keys from "./components/keypad/Keys";
 
 function App() {
   const [toggle, setToggle] = useState("theme1");
+  const [calc, setCalc] = useState<string[]>([]);
+  const [result, setResult] = useState<number>();
 
   const switchThemeHandler = (num: string) => {
     switch (num) {
@@ -20,12 +22,39 @@ function App() {
     }
   };
 
+  const getKeyValueHandler = (key: string) => {
+    if (key === "=") {
+      let c = calc.join("").replaceAll("x", "*");
+      console.log(BigInt(eval(c)));
+
+      let r = eval(c);
+
+      setResult(r);
+      setCalc([r.toString()]);
+    }
+    if (key === "DEL") {
+      setCalc((prev) => prev.slice(0, -1));
+      setResult(0);
+    }
+    if (key === "RESET") {
+      setCalc((prev) => []);
+      setResult(0);
+    }
+
+    // do not add to calc
+    if (key !== "=" && key !== "DEL" && key !== "RESET") {
+      setCalc((prev) => [...prev, key]);
+    }
+  };
+
   return (
     <div className={`${toggle} App`}>
       <div className="main">
         <Header switchThemeHandler={switchThemeHandler} />
-        <div className="screen"></div>
-        <Keys />
+        <div className="screen">
+          {result ? result.toLocaleString("en-US") : calc}
+        </div>
+        <Keys getKeyValueHandler={getKeyValueHandler} />
       </div>
     </div>
   );
