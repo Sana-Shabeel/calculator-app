f0b930c5d4be336157c8eca18f21aa870efa2e9f
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-21 17:03:00 +0300

    Add swicth theme func

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,35 @@
 import React from "react";
-import Theme from "./Theme";
 import "./header.scss";
 
-const Header = () => {
+interface Props {
+  switchThemeHandler: (theme: string) => void;
+}
+
+const Header = ({ switchThemeHandler }: Props) => {
   return (
     <div className="header">
       <h1>calc</h1>
       <div className="theme-word">
         <p>Theme</p>
-        <Theme />
+        <div className="theme-changer">
+          <div className="themes-level">
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+          </div>
+
+          <div className="dot-container">
+            <input
+              type="range"
+              name="slider"
+              min="1"
+              max="3"
+              defaultValue="1"
+              id="slider"
+              onChange={(e) => switchThemeHandler(e.target.value)}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
